0b8a2baa4c3a05930d680874ba36eb156a9c6bf5
Brian <brian.fly0623@gmail.com>
2022-12-22 14:06:53 -0300

    :sparkles: creating injection point on Service to create persistance methods with database

--- src/main/java/com/bibs/email/services/EmailService.java
+++ src/main/java/com/bibs/email/services/EmailService.java
@@ -1,5 +1,14 @@
 package com.bibs.email.services;
 
+import com.bibs.email.repositories.EmailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 // Service é a camada intermediária entre o Repository e o Controller
+@Service
 public class EmailService {
+
+    // ponto de injeção de repository para conseguir criar os métodos para fazer a persistência com o banco
+    @Autowired
+    EmailRepository emailRepository;
 }

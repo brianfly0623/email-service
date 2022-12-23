8d7aed916635c8918a7ccfb9458b0b5a1e9138df
Brian <brian.fly0623@gmail.com>
2022-12-23 14:03:11 -0300

    :sparkles: adding EmailService with method to send and save in our database

--- src/main/java/com/bibs/email/services/EmailService.java
+++ src/main/java/com/bibs/email/services/EmailService.java
@@ -1,8 +1,15 @@
 package com.bibs.email.services;
 
+import com.bibs.email.enums.StatusEmail;
+import com.bibs.email.models.EmailModel;
 import com.bibs.email.repositories.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 // Service é a camada intermediária entre o Repository e o Controller
 @Service
@@ -11,4 +18,25 @@ public class EmailService {
     // ponto de injeção de repository para conseguir criar os métodos para fazer a persistência com o banco
     @Autowired
     EmailRepository emailRepository;
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public void sendEmail(EmailModel emailModel) {
+        emailModel.setSendDateEmail(LocalDateTime.now());
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        } catch (MailException e) {
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+            return emailRepository.save(emailModel);
+        }
+    }
 }
